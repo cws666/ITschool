0ce56171045677a6896bdfd5d6a30dc4a93635df
陈文生 <2771628927@qq.com>
2020-08-11 15:29:29 +0800

    重新开始itschool项目
    优化js文件，滤清思路，重新开始弄itschool项目，为制作搜索引擎打下基础

--- js/zy.js
+++ js/zy.js
@@ -7,8 +7,8 @@ httpRequest.send();
 httpRequest.onreadystatechange = qq_fh;
 
 function qq_fh() {
-	if (httpRequest.readyState == 4 && httpRequest.status == 200) {
 		var sj = httpRequest.responseText; //获取并接受返回数据
+	if (httpRequest.readyState == 4 && httpRequest.status == 200) {
 		sj = JSON.parse(sj); //字符串转成对象
 		$("#jzz").remove();
 		//循环输出到页面
@@ -21,14 +21,6 @@ function qq_fh() {
 			xz1 = "<a id='a_" + i + "' onclick='down(" + i + ")' name='" + sj.text[i].id + "'>点我下载</a>"
 			xz = "<td class='xzcl'>" + xz1 + "</td>";
 			lb_id.innerHTML += "<tr>" + xh + mc + dx + lx + xz + "</tr>";
-			// lb_id.innerHTML+="<tr>";
-			// lb_id.innerHTML+="<th scope='row'>"+(i+1)+"</th>";
-			// lb_id.innerHTML+="<td>"+sj.text[i].name_all+"</td>";
-			// lb_id.innerHTML+="<td>"+sj.text[i].size+"</td>";
-			// lb_id.innerHTML+="<td>"+sj.text[i].icon+"</td>";
-			// lb_id.innerHTML+="<td>"+sj.text[i].id+"</td>";
-			// lb_id.innerHTML+="</tr>";
-			// lb_id.innerHTML+="</tbody>";
 
 		}
 
@@ -42,7 +34,7 @@ function down(xh_id) {
 	ajax_zhilian.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 	ajax_zhilian.send("url=https://cws.lanzous.com/" + document.getElementById("a_" + xh_id).name);
 	ajax_zhilian.onreadystatechange = function() {
-  
+
 		if (ajax_zhilian.readyState == 4 && ajax_zhilian.status == 200) {
 
 			console.log(ajax_zhilian.responseText);
